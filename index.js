daff53ef590350a789a14bef6383cda7064eb299
epointal <elisabeth.pointal@free.fr>
2019-05-29 14:59:22 +0200

    add an index.js to easyly import component

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Export components
+ */
+import FormaterDimensionBlock from './src/formater-dimension-block.vue'
+import FormaterContact from './src/formater-contact.vue'
+import FormaterDrawBbox from './src/formater-draw-bbox.vue'
+import FormaterFacetBlock from './src/formater-facet-block.vue'
+import FormaterListContact from './src/formater-list-contact.vue'
+import FormaterListMetadata from './src/formater-list-metadata.vue'
+import FormaterMap from './src/formater-map.vue'
+import FormaterOnLine from './src/formater-online'
+import FormaterPaging from './src/formater-paging.vue'
+import FormaterQuicklooks from './src/formater-quicklooks.vue'
+import FormaterSpatialSearch from './src/formater-spatial-search.vue'
+
+export {FormaterDimensionBlock, FormaterContact, FormaterDrawBbox,  FormaterFacetBlock,  FormaterListContact, 
+  FormaterListMetadata, FormaterMap, FormaterOnLine , FormaterPaging , FormaterQuicklooks , FormaterSpatialSearch  }
